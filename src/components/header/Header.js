f33cd10cbe2bac00bdb83743e7eded91b6e6ba34
Duc Khang Nguyen <duckhang2210@gmail.com>
2019-11-02 11:09:59 -0500

    fix logo bug

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
     return(
     <nav className="nav justify-content-center">
-        <img src="../../images/logo.png" width="150px" alt="Clicky Game"/>
+        <img src="../../images/logo.PNG" width="150px" alt="Clicky Game"/>
     </nav>
     )
 };
